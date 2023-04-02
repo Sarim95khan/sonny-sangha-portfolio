882334c4faa4c16c6d6aa24013248b314e5ad27e
Sarim95khan <sarim95khan@gmail.com>
2023-04-02 20:40:19 +0500

    added image and buttons

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 3. Install react-social-icons
    - Change bg/fg color with bgColor/ fgColor to gray & transparent
 4. Install react-simple-typewriter
+5. Create BackgroundCircles
+   - Animate via animate-pulse & framer-motion
+
+Colors:
+#242424
+#F7AB0A
+#F6AB0A
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
+import Image from "next/image";
 
 type Props = {};
 
@@ -21,10 +23,37 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col items-center justify-center text-center overflow-hidden space-y-8">
       <BackgroundCircles />
-      <h1>
-        {text}
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <Image
+        className=" relative rounded-full object-cover"
+        src="/images/Sarim.jpeg"
+        width={128}
+        height={128}
+        alt="Sarim"
+      />
+      <div className="z-20">
+        <h1 className="text-sm  uppercase text-gray-500 tracking-[5px]">
+          Chemical Engineer
+        </h1>
+        <h2 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h2>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">about</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">skills</button>
+          </Link>
+          <Link href="#project">
+            <button className="heroButton">Project"</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
